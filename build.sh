b69ad31fa97b654eb27cd5b63ee4c0b46e346c3f
frcooper <me@frcooper.com>
2025-11-16 00:24:47 -0500

    fix: Update date format in index.yml and modify release workflow to use UTC date

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd dist
-
-# Copy only what the plugin needs at runtime:
-cp ../plugin-version-links.yml .
-cp ../main.js .
-
-zip -r plugin-version-links-0.1.0.zip plugin-version-links.yml main.js
-
-sha256sum plugin-version-links-0.1.0.zip > plugin-version-links-0.1.0.zip.sha256
